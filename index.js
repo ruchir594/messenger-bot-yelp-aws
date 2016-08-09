d38e705715c9de9d15ccdacb76d3eaf5862ad947
ruchir594 <ruchir594@gmail.com>
2016-08-09 11:36:06 +0530

    making exact sopy of current

--- index.js
+++ index.js
@@ -38,49 +38,138 @@ app.post('/webhook', (req, res) => {
         if (event.postback) {
             const text = JSON.stringify(event.postback).substring(0, 200);
             sendTextMessage(sender, 'Postback received: ' + text);
-        } else if (event.message && event.message.text) {
+        } else if (event.message && event.message.text) { // bracket 101 open
             const text = event.message.text.trim().substring(0, 200);
 
-                sendTextMessage(sender, 'Text received, so gtfo?: ' + text);
-                var options = {
-                  mode: 'text',
-                  args: [text]
-                };
-                PythonShell.run("./lambda_function.py" , options,function (err, results) {
-                  if (err) throw err;
-                  //console.log('result: %j', results);
-                  console.log('back in app.js')
-                  console.log(results)
-                  var results = String(results)
-                  var places = results.split("@");
-                  //console.log(places)
-                  if (places[0] == 'jankiap50') {
-                      sendTextMessage(sender, places[1]);
-                      sendTextMessage(sender, "sorry, but i will soon be a lot smarter. Please come back in a week. Meanwhile, please please like the page.")
-                      //sendTextMessage(sender, places[2]);
-                      //sendTextMessage(sender, places[3])
-                  }
-                  else {
-                    var aplace = [];
-                    var aplacea="";
-                    var i = 0;
-                    var increment = 5;
-                    while (i < places.length){
-                        //aplacea = places[i]+"\n"+places[i+1]+"\n"+places[i+3]+"\n"+places[i+4];
-                        aplace.push(places[i]+"^"+places[i+1]+"^"+places[i+2]+"^"+places[i+3]+"^"+places[i+4]);
-                        if (typeof(places[i+1]) == typeof("hello")){
-                        //sendTextMessage(sender, aplacea);
-                        i=i+increment;
+            /////////////////////////////////////////////////////////////////////////////////////////
+
+                    var d1 = [];
+                    d1.push('i', 'live', 'in', 'please', 'hi', 'give', 'find', 'who', 'what', 'my', 'hungry', 'near', 'me', 'thank', 'you');
+                    d1.push('want', 'to', 'eat', 'like','liked', 'I', 'can', 'you', 'suggest', 'of', 'is', 'are', 'near', 'there', 'some');
+                    d1.push('little', 'now', 'wanna', 'want', 'at', 'on', 'in', 'near', 'area', 'next', 'and', 'how', 'about', 'or');
+                    d1.push('the', 'a', 'an', 'about', 'for', 'with', 'should', 'could', 'would', 'out','time','person','year','way','day');
+                    d1.push('thing','man','world','life','hand','part','child','eye','woman','place','work','week', 'doing');
+                    d1.push('case','point','government','company','number','group','problem','fact','be','have','do','say');
+                    d1.push('get','make','go','know','take','see','come','think','look','want','give','use','find','tell', 'telling');
+                    d1.push('ask','work','seem','feel','try','leave','call','good','new','first','last','long','great','little','own','other');
+                    d1.push('old','right','big','high','different','small','large','next','early','young','important','few');
+                    d1.push('public','bad','same','able','to','of','in','for','on','with','at','by','from','up','about','into');
+                    d1.push('over','after','beneath','under','above','the','and','a','that','I','it','not','he','as','you');
+                    d1.push('this','but','his','they','her','she','or','an','will','my','one','all','would','there','their', 'talk');
+                    d1.push('talking', 'love', 'loved', 'hello', 'help', 'helping', 'helped', 'pleasure', 'bye', 'goodbye', 'care', 'later');
+                    d1.push('no','nothing', 'thanks', 'welcome', 'something', 'smart', 'dumb', 'poor', 'am', 'hey');
+
+                    var text2 = text.replace(/\?/g,'');
+                    text2 = text2.replace(/!/g,'');
+                    text2 = text2.replace(/\./g,'');
+                    text2 = text2.replace(/,/g,'');
+                    text2 = text2.replace(/:/g,'');
+
+                    console.log("Receive a message2: " + text2);
+
+                    var words_text = text2.split(' ');
+
+                    var i = 0
+                    var flag = false
+                    var hasher = -1
+                    while(i<words_text.length){
+                      hasher = d1.indexOf(words_text[i].toLowerCase())
+                      if(hasher == -1){
+                        flag = true
+                        console.log("Flag true on: " + words_text[i]);
+                      }
+                      i = i + 1
+                    }
+                    if(flag == false){
+
+                      if(words_text[0].toLowerCase() == 'hi' || words_text[0].toLowerCase() == 'hello'){
+                        sendTextMessage(sender, "Hello, I am Natasha.")
+                      }
+                      else if(words_text[0].toLowerCase() == 'who'){
+                        sendTextMessage(sender, "Hi there, My name is Natasha and I am a smart AI.")
+                      }
+                      else if(words_text[0].toLowerCase() == 'what'){
+                        sendTextMessage(sender, "I am smart bot that uses Yelp API to help you find food in your area. Please tell me your city and what you want to eat. I will soon be smarter than I am, please come back in a week.")
+                        sendTextMessage(sender, "Meanwhile, please please like my page.")
+                      }
+                      else if(words_text[0].toLowerCase() == 'how'){
+                        sendTextMessage(sender, "Let's just say I am smart...")
+                        sendTextMessage(sender, "Meanwhile, please please like my page.")
+                      }
+                      else if(words_text[0].toLowerCase() == 'goodbye' || words_text.indexOf('bye') != -1){
+                        sendTextMessage(sender, "Bye, thank you for dropping by.")
+                        sendTextMessage(sender, "Meanwhile, please please like my page.")
+                      }
+                      else if(words_text[0].toLowerCase() == 'thank' || words_text[0].toLowerCase() == 'thanks'){
+                        sendTextMessage(sender, "You are welcome.")
+                        sendTextMessage(sender, "Meanwhile, please please like my page.")
+                      }
+                      else if(words_text[0].toLowerCase() == 'you'){
+                        sendTextMessage(sender, "Well, you have strong opinions about me.")
+                        sendTextMessage(sender, "Meanwhile, please please like my page.")
+                      }
+                      else if(words_text[0].toLowerCase() == 'i' && words_text[1].toLowerCase() == 'want'){
+                        sendTextMessage(sender, "I cannot tell what you want, please be more specific and give me your location as well.  I will soon be smarter than I am, please come back in a week.")
+                      }
+                      else if(words_text[0].toLowerCase() == 'i' && words_text[1].toLowerCase() == 'am'){
+                        sendTextMessage(sender, "Sure, you are!")
+                        sendTextMessage(sender, "Meanwhile, please please like my page.")
+                      }
+                      else if(words_text[0].toLowerCase() == 'i'){
+                        sendTextMessage(sender, "Okay, maybe!")
+                      }
+                      else {
+                        sendTextMessage(sender, "I am just front end Yelp API integration, I am not allowed to do anything else. Give me the food you want and city/town you are in. I will soon be smarter than I am, please come back in a week. I just got a but smarter. ")
+                        sendTextMessage(sender, "Meanwhile, please please like my page.")
+                      }
+
+                      //sendTextMessage(sender, "Nothing to Process...")
+
+
+                    } else { //bracket 102 open
+                      sendTextMessage(sender, "processing...")
+
+                      //sendTextMessage(sender, 'Text received, so gtfo?: ' + text);
+                      var options = {
+                        mode: 'text',
+                        args: [text]
+                      };
+                      PythonShell.run("./lambda_function.py" , options,function (err, results) {
+                        if (err) throw err;
+                        //console.log('result: %j', results);
+                        console.log('back in app.js')
+                        console.log(results)
+                        var results = String(results)
+                        var places = results.split("@");
+                        //console.log(places)
+                        if (places[0] == 'jankiap50') {
+                            sendTextMessage(sender, places[1]);
+                            sendTextMessage(sender, "sorry, but i will soon be a lot smarter. Please come back in a week. Meanwhile, please please like the page.")
+                            //sendTextMessage(sender, places[2]);
+                            //sendTextMessage(sender, places[3])
                         }
                         else {
-                        i=i+increment;
+                          var aplace = [];
+                          var aplacea="";
+                          var i = 0;
+                          var increment = 5;
+                          while (i < places.length){
+                              //aplacea = places[i]+"\n"+places[i+1]+"\n"+places[i+3]+"\n"+places[i+4];
+                              aplace.push(places[i]+"^"+places[i+1]+"^"+places[i+2]+"^"+places[i+3]+"^"+places[i+4]);
+                              if (typeof(places[i+1]) == typeof("hello")){
+                              //sendTextMessage(sender, aplacea);
+                              i=i+increment;
+                              }
+                              else {
+                              i=i+increment;
+                              }
+                          }
+                          sendGenericMessage(sender, aplace);
                         }
-                    }
-                    sendGenericMessage(sender, aplace);
-                  }
-                });
+                      });
+              } //bracket 102 close
 
-        }
+        } //bracket 101 close
     });
 
     res.sendStatus(200);
