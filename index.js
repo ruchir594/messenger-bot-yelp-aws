a8b8abf52d829ec022c6ad8acb269658e9682865
ruchir594 <ruchir594@gmail.com>
2016-12-06 11:21:51 +0530

    removing simple NLG in JS 2

--- index.js
+++ index.js
@@ -43,48 +43,6 @@ app.post('/webhook', (req, res) => {
             const text = event.message.text.trim().substring(0, 200);
 
             /////////////////////////////////////////////////////////////////////////////////////////
-
-                    var d1 = [];
-                    d1.push('i', 'live', 'in', 'please', 'hi', 'give', 'find', 'who', 'what', 'my', 'hungry', 'near', 'me', 'thank', 'you');
-                    d1.push('want', 'to', 'eat', 'like','liked', 'I', 'can', 'you', 'suggest', 'of', 'is', 'are', 'near', 'there', 'some');
-                    d1.push('little', 'now', 'wanna', 'want', 'at', 'on', 'in', 'near', 'area', 'next', 'and', 'how', 'about', 'or');
-                    d1.push('the', 'a', 'an', 'about', 'for', 'with', 'should', 'could', 'would', 'out','time','person','year','way','day');
-                    d1.push('thing','man','world','life','hand','part','child','eye','woman','place','work','week', 'doing');
-                    d1.push('case','point','government','company','number','group','problem','fact','be','have','do','say');
-                    d1.push('get','make','go','know','take','see','come','think','look','want','give','use','find','tell', 'telling');
-                    d1.push('ask','work','seem','feel','try','leave','call','good','new','first','last','long','great','little','own','other');
-                    d1.push('old','right','big','high','different','small','large','next','early','young','important','few');
-                    d1.push('public','bad','same','able','to','of','in','for','on','with','at','by','from','up','about','into');
-                    d1.push('over','after','beneath','under','above','the','and','a','that','I','it','not','he','as','you');
-                    d1.push('this','but','his','they','her','she','or','an','will','my','one','all','would','there','their', 'talk');
-                    d1.push('talking', 'love', 'loved', 'hello', 'help', 'helping', 'helped', 'pleasure', 'bye', 'goodbye', 'care', 'later');
-                    d1.push('no','nothing', 'thanks', 'welcome', 'something', 'smart', 'dumb', 'poor', 'am', 'hey');
-
-                    var text2 = text.replace(/\?/g,'');
-                    text2 = text2.replace(/!/g,'');
-                    text2 = text2.replace(/\./g,'');
-                    text2 = text2.replace(/,/g,'');
-                    text2 = text2.replace(/:/g,'');
-
-                    console.log("Receive a message2: " + text2);
-
-                    var words_text = text2.split(' ');
-
-                    var i = 0
-                    var flag = false
-                    var hasher = -1
-                    while(i<words_text.length){
-                      hasher = d1.indexOf(words_text[i].toLowerCase())
-                      if(hasher == -1){
-                        flag = true
-                        console.log("Flag true on: " + words_text[i]);
-                      }
-                      i = i + 1
-                    }
-                     //bracket 102 open
-                      //sendTextMessage(sender, "processing...")
-
-                      //sendTextMessage(sender, 'Text received, so gtfo?: ' + text);
                       var options = {
                         mode: 'text',
                         args: [text, sender]
@@ -99,9 +57,6 @@ app.post('/webhook', (req, res) => {
                         //console.log(places)
                         if (places[0] == 'jankiap50') {
                             sendTextMessage(sender, places[1]);
-                            //sendTextMessage(sender, "Hmmmm, didnt get what you are saying. Please come back in a week. Meanwhile, please please like the page.")
-                            //sendTextMessage(sender, places[2]);
-                            //sendTextMessage(sender, places[3])
                         }
                         else {
                           var aplace = [];
@@ -130,7 +85,6 @@ app.post('/webhook', (req, res) => {
 
         } //bracket 101 close
     });
-
     res.sendStatus(200);
 });
 
